b5839bfd44102e68e8b488d9970ea1eea6c29387
Paul Cabot <paul.claude.cabot@gmail.com>
2024-11-13 14:31:07 +0100

    MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Creating restaurants..."
+
+Restaurant.destroy_all
+
+Restaurant.create!(name: "Master Poulet", address: "1 rue des poulets", phone_number: "06 62 46 01 02", category: "french")
+Restaurant.create!(name: "Come Prima", address: "23 rue de Rome", phone_number: "03 83 35 22 41", category: "italian")
+Restaurant.create!(name: "Yi Fang", address: "7 rue des Archives", phone_number: "01 42 08 19 74", category: "chinese")
+Restaurant.create!(name: "Chez Justine", address: "4 rue d'Oberkampf", phone_number: "01 60 37 62 48", category: "belgian")
+Restaurant.create!(name: "Kyobashi", address: "8 rue du dragon", phone_number: "01 39 51 03 03", category: "japanese")
+
+puts "Finished! Created #{Restaurant.count} restaurants."
